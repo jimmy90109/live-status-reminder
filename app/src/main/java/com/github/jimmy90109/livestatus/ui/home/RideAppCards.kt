23ef79bed3c71652e5b50a489881904d6570d4ed
jimmy90109 <85556680+jimmy90109@users.noreply.github.com>
2026-08-02 15:49:18 +0800

    Use car icon for Uber ride card fallback

--- app/src/main/java/com/github/jimmy90109/livestatus/ui/home/RideAppCards.kt
+++ app/src/main/java/com/github/jimmy90109/livestatus/ui/home/RideAppCards.kt
@@ -1,68 +1,20 @@
 package com.github.jimmy90109.livestatus.ui.home
 
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.drawable.Drawable
-import android.os.SystemClock
-import androidx.annotation.DrawableRes
-import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Switch
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.graphics.painter.BitmapPainter
-import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.semantics.Role
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.github.jimmy90109.livestatus.AppReminderPreferences
 import com.github.jimmy90109.livestatus.BuildConfig
-import com.github.jimmy90109.livestatus.ClockTimerNotificationExtractor
-import com.github.jimmy90109.livestatus.ClockTimerSource
-import com.github.jimmy90109.livestatus.ClockTimerState
-import com.github.jimmy90109.livestatus.ClockTimerUpdate
 import com.github.jimmy90109.livestatus.LiveStatusNotificationParser
 import com.github.jimmy90109.livestatus.LiveStatusReminder
 import com.github.jimmy90109.livestatus.R
 import com.github.jimmy90109.livestatus.ui.theme.LocalAppColors
-import kotlinx.coroutines.launch
-
 
 
 @Composable
@@ -78,7 +30,7 @@ internal fun UberRideCard(
     AppCard(
         appName = "Uber",
         appPackageName = UBER_PACKAGE,
-        fallbackIconRes = R.drawable.ic_notification,
+        fallbackIconRes = R.drawable.ic_car_notification,
         title = "乘車進度",
         description = "一般行程顯示上車點、車輛與 PIN；優步小黃顯示職業駕駛接近進度。",
         supportedLanguages = listOf("繁中 / En"),
